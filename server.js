50ee7f83f8df97af2e1d271040d50534fbc0c2e0
Fatih Mehmet <tuncer.fm@gmail.com>
2022-02-06 16:56:04 +0300

    changes maked

---
+++ server.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const app=express();
+const http = require('http')
+const port=8080;
+
+const apiKey = `700e32c17064884435e55e54918aba1c`;
+
+
+
+app.listen(port,()=>{
+    console.log('live on port '+port);
+});
+
+
+
+app.get('/',function(req,res){
+    res.send({
+        "firstname": "Fatih Mehmet",
+        "lastname": "Tuncer"
+        });
+   
+});
+
+
+app.get('/temperature',(req,res)=>{
+    const apikey = '700e32c17064884435e55e54918aba1c';
+    var query = require('url').parse(req.url,true).query;
+    var city = query.city;
+    const url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city + "&appid=" + apikey + "&units=metric";
+    	
+    http.get(url, (response) => {
+        if (response.statusCode === 200) {
+          response.on("data", (data) => {
+            const weatherData = JSON.parse(data);
+            console.log('index', {data: weatherData});
+          })
+        } else {
+          res.send('index', {data: "0"})
+        }
+      })
+    
+    
+	res.send(response);
+
+
+    console.log(city);
+    var response=req.query;
+    
+
+    console.log(query);
+    console.log(query);
+    res.send(response);
+});
+
+
